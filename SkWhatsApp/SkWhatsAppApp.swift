9e37b4b072951e37143d8ad6e3779dfc5cb4896d
sushil-mahto <sushilkr561998@gmail.com>
2022-02-17 12:31:16 +0530

    Initial Commit

---
+++ SkWhatsApp/SkWhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkWhatsAppApp.swift
+//  SkWhatsApp
+//
+//  Created by SUshil on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SkWhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
